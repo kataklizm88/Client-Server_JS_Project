70781cc863e38ab2e8471080b9db3ffc5c0a8179
Vlad <mitelenager@mail.ru>
2022-12-04 16:31:23 +0300

    Project is ready. Next step is webpack/

--- Shop/index.js
+++ Shop/index.js
@@ -1,14 +1,22 @@
 const URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json'
+const BASE_URL = "http://localhost:8000"
+const GET_GOODS_ITEMS = `${BASE_URL}/goods.json`
+const GET_BASKET_GOODS = `${BASE_URL}/basket_goods`
 
-const service = (url) => fetch(url)
-    .then((res)=> {
-        return res.json()
-     })
 
+function service(url, method="GET", body) {
+  return fetch(url, {
+    headers: Object.assign({}, body ? {
+      'Content-Type': 'application/json; charset=utf-8'
+    } : {}),
+    method,
+    body: JSON.stringify(body)
+  })
+  .then((res) => res.json())
+}
 
 
 function init(){
-
     Vue.component('nav-comp',{
         template:`
             <div class="navbar navbar-expand-lg navbar-light bg-info">
@@ -25,8 +33,6 @@ function init(){
             </div>`
     })
 
-
-
     Vue.component('basket',{
         template:`
         <div class="fixed-area" >
@@ -34,8 +40,7 @@ function init(){
         </div>`
     })
 
-
-    Vue.component('item-card',{
+    Vue.component('goods-item',{
         props:[
             'item'
         ],
@@ -43,87 +48,134 @@ function init(){
         <div class="col-sm-3 p-4 border bg-warning" >
             <h5 class="card-title">{{item.product_name}}</h5>
             <p class="card-text">{{item.price}}</p>
-            <a href="#" class="btn btn-primary">Купить товар</a>
-        </div>`
+            <custom-button  class="btn btn-primary" @click="addGood" button_name='Добавить' ></custom button>
+        </div>`,
+        methods: {
+            addGood(){
+                service(GET_BASKET_GOODS, 'PUT', {
+                    id : this.item.id
+                })
+            }
+        }
     })
 
     Vue.component('basket-item',{
+        data(){
+            return {
+                basketGoodsItems : []
+            }
+        },
         template:`
             <div class="basket-card">
                 <div style="text-align:right ">
                     <button type="button"  class="btn-close" aria-label="Close" v-on:click="$emit('my_click')"></button>
-                </div>  
+                </div>
                <div class="basket-card__header">
                   <h1 class="basket-card__header__title">basket card</h1>
                   <div class="basket-card__header__delete-icon"> </div>
                </div>
                <div class="basket-card__content">
-                  content
+                   <basket-goods-item v-for="item in basketGoodsItems" :item="item" @delete="deleteBasketGood" @add="addGood"></basket-goods-item>
                </div>
-            </div>`           
+            </div>`,
+        mounted(){
+            service(GET_BASKET_GOODS).then((data)=>{
+                this.basketGoodsItems = data
+            })
+        },
+        methods: {
+            deleteBasketGood(id) {
+              service(GET_BASKET_GOODS, "DELETE", {
+                id
+              }).then((data) => {
+                this.basketGoodsItems = data
+              })
+            },
+            addGood(id) {
+              service(GET_BASKET_GOODS, 'PUT', {
+                id
+              }).then((data) => {
+                this.basketGoodsItems = data
+              })
+            }
+          }
     })
 
-    Vue.component('search',{
-        props:['searchline'],
-        template:`
-            <div class="d-flex">
-                <slot> </slot>
-                <custom-button class="btn btn-outline-success" @some_click="filterGoods" button_name='Искать'> </custom-button>
-            </div>`            
-    })
-
-
+    Vue.component('basket-goods-item', {
+        props: [
+          'item'
+        ],
+        template: `
+          <div class="basket-card__content___item">
+             <h3>{{item?.data?.product_name}}</h3>
+             <div>count: {{item?.count}}</div>
+             <div>total: {{item?.total}}</div>
+             <custom-button class="btn btn-primary" @click="$emit('add', item.data.id)" button_name='Добавить'>добавить</custom-button>
+             <custom-button class="btn btn-primary" @click="$emit('delete', item.data.id)" button_name='Удалить'>удалить</custom-button>
+          </div>
+        `
+      })
+   
     Vue.component('custom-button',{
         props:['button_name'],
         template:`
-            <button  type="button" v-on:click="$emit('some_click')" >{{button_name}}</button>`
+            <button type="button" v-on:click="$emit('click')">
+                {{button_name}}
+            </button>`
     })
 
-
+    Vue.component('search',{
+        props:[
+            'value'
+        ],
+        template:`
+            <div class="d-flex">
+                <input type="text" class="form-control me-2" placeholder="Search" aria-label="Search" :value="value" @input="$emit('input', $event.target.value)"/>	
+               
+            </div>`
+    })
 
     const app = new Vue({
         el: '#app',
         data: {
             name: "Jack",
             items: [],
-            filtederItems: [],
             searchline: '',
             isVisibleCart: false,
         },
-
         methods: {
             fetchGoods(){
-                return service(URL).then((data)=>{
+                return service(GET_GOODS_ITEMS).then((data)=>{
                     this.items = data;
-                    this.filteredItems = data
                 })
             },
 
             setVisionCard() {
                 this.isVisibleCart = !this.isVisibleCart
             },
-
-            filterGoods(){
-                this.filteredItems = this.items.filter(({ product_name }) => {
-                return product_name.match(new RegExp(this.search, 'gui'))
-                })
-            },
-
+            
             visibleCartOn(){
                 this.isVisibleCart = true
-                console.log(this.isVisibleCart)
             },
 
             visibleCartOff(){
                 this.isVisibleCart = false
+            },
+            onSearchComponent(value){
+                this.searchline = value
             }
         },
         computed: {
             get_total() {
-            return this.filteredItems.reduce((prev, { price }) => {
+            return this.items.reduce((prev, { price }) => {
               return prev + price;
-            }, 0)
-          }
+                }, 0)
+            },
+            filterGoods(){
+                return this.items.filter(({ product_name }) => {
+                    return product_name.match(new RegExp(this.searchline, 'gui'))
+                    })
+                },
         },
         mounted(){
             this.fetchGoods()
